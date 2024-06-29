afb837adc3a81a31e22892773192886157b12023
Kafui Alordo <kalordo7@gmail.com>
2024-06-29 23:02:43 +0000

    First commit

---
+++ apis/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+# from .views import ListTodo, DetailTodo
+from .views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('', TodoViewSet, basename='todos')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', ListTodo.as_view()),
+#     path('<int:pk>/', DetailTodo.as_view()),
+# ]
